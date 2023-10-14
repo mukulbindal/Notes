4e6f07a4c3bd00167127993a4598a32dfee1176d
mukulbindal <mukulbindal170299@gmail.com>
2023-10-14 20:55:18 +0530

    added docs

--- test.py
+++ test.py
@@ -1,11 +1,27 @@
-cap, mil, dis = map(int, input().split(","))
+def robot_collisions(N, A):
+    # Create a list of robots with their health and direction
+    robots = [(health, direction) for health, direction in A]
 
-travelled = 0
-fuel = 0
-if(cap*mil >= dis):
-    while travelled < 1000:
-        fuel+=1
-        travelled += cap * mil 
-    print("Yes", fuel)
-else:
-    print("No")
+    while True:
+        collisions = []
+        for i in range(N - 1):
+            if robots[i][1] == 1 and robots[i + 1][1] == 0:
+                # Robots facing right collide with robots facing left
+                if robots[i][0] > robots[i + 1][0]:
+                    collisions.append(i + 1)
+                elif robots[i][0] < robots[i + 1][0]:
+                    collisions.append(i)
+                else:
+                    collisions.extend([i, i + 1])
+
+        if not collisions:
+            break   # No more collisions
+
+        # Remove robots involved in collisions
+        robots = [robot for index, robot in enumerate(
+            robots) if index not in collisions]
+        N = len(robots)  # Update the number of robots
+
+    return robots
+
+print(robot_collisions(4, [(39,1),(20, 0), (21,0), (12,0)]))
